9044a9ebf765d96f2f4d76e0822f1fb5635154cb
riy0 <chac0ptx@gmail.com>
2019-08-16 07:52:14 +0900

    get trends

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -3,15 +3,16 @@ class GithubsController < ApplicationController
   require "nokogiri"
 
   def index
-    url = "https://github.com/trending"
-    doc = Nokogiri.HTML(open(url))
-    @contents = doc.xpath('//h3').css('a')
+    url = open("https://github.com/trending")
+    doc = Nokogiri::HTML.parse(url, nil, "UTF-8")
+    puts doc.xpath('//h1').css('a')
+    @contents = doc.xpath('//h1').css('a')
     @stars = doc.xpath('//span[@class="d-inline-block float-sm-right"]')
   end
 
   def show
     @url = "https://github.com" + params[:repo]
-    doc = Nokogiri.HTML(open(@url))
+    doc = Nokogiri::HTML(open(@url))
     @title = params[:title]
     @detail = doc.xpath('//div[@class="js-repo-meta-container"]').css('span')
     @tags = doc.xpath('//div[@class="js-repo-meta-container"]').css('a')
@@ -20,9 +21,9 @@ class GithubsController < ApplicationController
 
   def developers
     url = "https://github.com/trending/developers"
-    doc = Nokogiri.HTML(open(url))
-    @contents = doc.xpath('//h2').css('a')
-    @images = doc.xpath('//img') 
+    doc = Nokogiri::HTML(open(url))
+    @contents = doc.xpath('//h1').css('a')
+    @images = doc.xpath('//img[@class="avatar mb-1"]')
   end
 
 end
